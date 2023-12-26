5c4f228a8139b64d61ee5190b8418f1c3c98d129
Emilymatoke <emilymomata@gmail.com>
2023-12-26 13:29:37 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a+b;
+}
+function subtract(a, b) {
+    return a-b;
+}
+function multiply(a, b) {
+    return a*b;
+}
+function divide(a, b) {
+    return a / b;
+  }
+  function increment(a) {
+    return a + 1;
+  }
+  function decrement(a) {
+    return a - 1;
+  }
+  
+  function makeInt(a) {
+    return parseInt(a, 10);
+  }
+  makeInt("124"); // returns 123
+  makeInt("124.45"); // returns 123
+  makeInt("Andrewkyalonzioki"); // returns NaN
+  
+  function preserveDecimal(a) {
+    return parseFloat(a);
+  }
+  preserveDecimal(23.003);
+  preserveDecimal("darn");
